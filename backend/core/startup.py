eee2b756b6f6965b0ac39475f7dc3c145fdb1a37
jisha charly <jisha.charly@gmail.com>
2026-03-06 15:09:22 +0530

    fix duplicate super admin creation

--- backend/core/startup.py
+++ backend/core/startup.py
@@ -2,17 +2,18 @@ import os
 from django.contrib.auth import get_user_model
 from apps.users.models import Role
 
-def create_super_admin():
-    username = os.getenv("SUPERADMIN_USERNAME")
-    email = os.getenv("SUPERADMIN_EMAIL")
-    password = os.getenv("SUPERADMIN_PASSWORD")
 
-    if not username or not password:
-        return
+def create_super_admin():
+    username = os.getenv("SUPERADMIN_USERNAME", "admin")
+    email = os.getenv("SUPERADMIN_EMAIL", "admin@gmail.com")
+    password = os.getenv("SUPERADMIN_PASSWORD", "Admin@123")
 
     User = get_user_model()
 
-    if not User.objects.filter(username=username).exists():
+    # check if user already exists
+    user = User.objects.filter(email=email).first()
+
+    if not user:
         user = User.objects.create_superuser(
             username=username,
             email=email,
@@ -24,3 +25,6 @@ def create_super_admin():
         user.save()
 
         print("Super Admin created")
+
+    else:
+        print("Super Admin already exists")
